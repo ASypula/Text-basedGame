925e0744e7dac4bfd9af48e34a3066d0399d1e05
Byku <sztanga.eryk@gmail.com>
2023-03-10 15:46:36 +0100

    Put rules in separate file

--- obstaclesDemo.pl
+++ obstaclesDemo.pl
@@ -4,9 +4,12 @@
 :- retractall(at(_, _)), retractall(i_am_at(_)), retractall(alive(_)), retractall(blocked(_)), retractall(holding(_)). /* CHANGED HERE*/
 /*:- ensure_loaded(rooms/room_1), ensure_loaded(rooms/room_2), ensure_loaded(rooms/room_3), ensure_loaded(rooms/room_4).*/
 
+:- ensure_loaded(rules/rules).
+
 /* to add obstacle logic to main game copy unlock, go and look rules. Add apropriate path, subroom, blocked and describe additional truths.*/
 
 i_am_at(room1).
+holding(lantern).
 
 path(room1, n, blocked_room).
 path(room1, n, blocked_roomS).
@@ -32,142 +35,6 @@ at(bucket, blocked_roomS).
 subroom(blocked_roomN, blocked_room). /* NEW */
 subroom(blocked_roomS, blocked_room). 
 
-/* NEW */
-/* These rules describe how to unlock closed path. */
-unlock :- 
-        at(Item, Place),
-        subroom(Place, UnlockedPlace),
-        retract(at(Item, Place)),
-        assert(at(Item, UnlockedPlace)),
-        false.
-
-unlock :-
-        i_am_at(Place),
-        subroom(Place, UnlockedPlace),
-        retract(i_am_at(Place)),
-        assert(i_am_at(UnlockedPlace)),
-        retract(blocked(UnlockedPlace)),
-        write("Unlocked"), nl.
-
-/* These rules describe how to pick up an object. */
-
-take(X) :-
-        holding(X),
-        write('You''re already holding it!'),
-        !, nl.
-
-take(X) :-
-        i_am_at(Place),
-        at(X, Place),
-        retract(at(X, Place)),
-        assert(holding(X)),
-        write('OK.'),
-        !, nl.
-
-take(_) :-
-        write('I don''t see it here.'),
-        nl.
-
-
-/* These rules describe how to put down an object. */
-
-drop(X) :-
-        holding(X),
-        i_am_at(Place),
-        retract(holding(X)),
-        assert(at(X, Place)),
-        write('OK.'),
-        !, nl.
-
-drop(_) :-
-        write('You aren''t holding it!'),
-        nl.
-
-
-/* These rules define the direction letters as calls to go/1. */
-
-n :- go(n).
-
-s :- go(s).
-
-e :- go(e).
-
-w :- go(w).
-
-
-/* This rule tells how to move in a given direction. */
-
-go(Direction) :-  /* CHANGED HERE*/
-        i_am_at(Here),
-        path(Here, Direction, There),
-        \+ blocked(There),
-        retract(i_am_at(Here)),
-        assert(i_am_at(There)),
-        !, look.
-
-go(_) :-
-        write('You can''t go that way.').
-
-
-/* This rule tells how to look about you. */
-
-look :-         /* CHANGED HERE*/
-        i_am_at(Place),
-        describe(Place), !,
-        describe_additional(Place), nl,
-        notice_objects_at(Place),
-        nl.
-
-
-/* These rules set up a loop to mention all the objects
-   in your vicinity. */
-
-notice_objects_at(Place) :-
-        at(X, Place),
-        write('There is a '), write(X), write(' here.'), nl,
-        fail.
-
-notice_objects_at(_).
-
-
-/* This rule tells how to die. */
-
-die :-
-        finish.
-
-
-/* Under UNIX, the "halt." command quits Prolog but does not
-   remove the output window. On a PC, however, the window
-   disappears before the final output can be seen. Hence this
-   routine requests the user to perform the final "halt." */
-
-finish :-
-        nl,
-        write('The game is over. Please enter the "halt." command.'),
-        nl.
-
-
-/* This rule just writes out game instructions. */
-
-instructions :-
-        nl,
-        write('Enter commands using standard Prolog syntax.'), nl,
-        write('Available commands are:'), nl,
-        write('start.             -- to start the game.'), nl,
-        write('n.  s.  e.  w.     -- to go in that direction.'), nl,
-        write('take(Object).      -- to pick up an object.'), nl,
-        write('drop(Object).      -- to put down an object.'), nl,
-        write('look.              -- to look around you again.'), nl,
-        write('instructions.      -- to see this message again.'), nl,
-        write('halt.              -- to end the game and quit.'), nl,
-        nl.
-
-
-/* This rule prints out instructions and tells where you are. */
-
-start :-
-        instructions,
-        look.
 
 
 /* These rules describe the various rooms.  Depending on
---
+++ rules/rules.pl
@@ -0,0 +1,115 @@
+/* to add obstacle logic to main game copy unlock, go and look rules. Add apropriate path, subroom, blocked and describe additional truths.*/
+
+/* NEW */
+/* These rules describe how to unlock closed path. */
+unlock :- 
+        at(Item, Place),
+        subroom(Place, UnlockedPlace),
+        retract(at(Item, Place)),
+        assert(at(Item, UnlockedPlace)),
+        false.
+
+unlock :-
+        i_am_at(Place),
+        subroom(Place, UnlockedPlace),
+        retract(i_am_at(Place)),
+        assert(i_am_at(UnlockedPlace)),
+        retract(blocked(UnlockedPlace)),
+        write("Unlocked"), nl.
+
+/* These rules describe how to pick up an object. */
+
+take(X) :-
+        holding(X),
+        write('You''re already holding it!'),
+        !, nl.
+
+take(X) :-
+        i_am_at(Place),
+        at(X, Place),
+        retract(at(X, Place)),
+        assert(holding(X)),
+        write('OK.'),
+        !, nl.
+
+take(_) :-
+        write('I don''t see it here.'),
+        nl.
+
+
+/* These rules define the direction letters as calls to go/1. */
+
+n :- go(n).
+
+s :- go(s).
+
+e :- go(e).
+
+w :- go(w).
+
+
+/* This rule tells how to move in a given direction. */
+
+go(Direction) :-  /* CHANGED HERE*/
+        i_am_at(Here),
+        path(Here, Direction, There),
+        \+ blocked(There),
+        retract(i_am_at(Here)),
+        assert(i_am_at(There)),
+        !, look.
+
+go(_) :-
+        write('You can''t go that way.').
+
+
+/* This rule tells how to look about you. */
+
+look :-         /* CHANGED HERE*/
+        i_am_at(Place),
+        describe(Place), !,
+        describe_additional(Place), nl,
+        notice_objects_at(Place),
+        nl.
+
+
+/* These rules set up a loop to mention all the objects in your vicinity. */
+
+notice_objects_at(Place) :-
+        at(X, Place),
+        write('There is a '), write(X), write(' here.'), nl,
+        fail.
+
+notice_objects_at(_).
+
+
+
+/* This rule just writes out game instructions. */
+
+instructions :-
+        nl,
+        write('Enter commands using standard Prolog syntax.'), nl,
+        write('Available commands are:'), nl,
+        write('start.             -- to start the game.'), nl,
+        write('n.  s.  e.  w.     -- to go in that direction.'), nl,
+        write('take(Object).      -- to pick up an object.'), nl,
+        write('inventory.         -- to see all picked up objects.'), nl,
+        write('look.              -- to look around you again.'), nl,
+        write('instructions.      -- to see this message again.'), nl,
+        write('halt.              -- to end the game and quit.'), nl,
+        write('unlock.            -- debug command to bypass example obstacle in demo.'), nl,
+        nl.
+
+
+/* This rule prints inventory contents */
+inventory :- write('Your objects:'), nl, fail.
+inventory :-
+        holding(Item),
+        write('    '), write(Item), nl, false.
+inventory :- true.
+
+
+/* This rule prints out instructions and tells where you are. */
+
+start :-
+        instructions,
+        look.
