f6c894c5b8427e5740651e3dae0634469083fb47
asypula <a.sypula@stud.elka.pw.edu.pl>
2023-03-10 20:58:33 +0100

    Renamed functions in objects, file for spells added

--- prolog/objects.pl
+++ prolog/objects.pl
@@ -1,64 +1,79 @@
-:- dynamic
-    possess/1.
-
 /* Get information about objects */
 
-check(old_journal) :-
-    possess(old_journal),
+describe(old_journal) :-
+    holding(old_journal),
     write("I found the exit! It is to the north, but fierce beast guides it. I'm gonna await next student who's gonna fail their exams and team up with them against the beast."),
     nl, !.
 
-check(firefly) :-
-    possess(firefly),
-    write("TODO"),
+describe(firefly) :-
+    holding(firefly),
+    write("Small, light-emitting beetles."),
     nl, !.
 
-check(burned_journal) :-
-    possess(burned_journal),
+describe(burned_journal) :-
+    holding(burned_journal),
     write("[illegible] is giving me a hard time. I know the [illegible] spell would be a save way to deal with it, however casting \"grab\" on it's tail would be funnier. Besides I don't have nightcap for [illegible] spell"),
     nl, !.
 
-check(jar) :-
-    possess(jar),
-    write("TODO"),
+describe(jar) :-
+    holding(jar),
+    write("Empty, a bit dusty jar."),
     nl, !.
 
 /* TODO: should we hint that an object is a spell component? */
-check(key) :-
-    possess(key),
+describe(key) :-
+    holding(key),
     write("Small iron key fit more for pendants than doors or chests. (hint: spell component)"),
     nl, !.
 
-check(cheat_sheet) :-
-    possess(cheat_sheet),
-    write("TODO"),
+metal(key).
+
+describe(cheat_sheet) :-
+    holding(cheat_sheet),
+    write("@TODO"),
     nl, !.
 
-check(magnet) :-
-    possess(magnet),
-    write("TODO"),
+describe(magnet) :-
+    holding(magnet),
+    write("Medium size magnet with carved initials."),
     nl, !.    
 
-check(rope) :-
-    possess(rope),
+describe(rope) :-
+    holding(rope),
     write("Old tattered rope. You wouldn't trust it to hold you weight"),
     nl, !. 
 
-check(beer) :-
-    possess(magnet),
+describe(beer) :-
+    holding(magnet),
     write("[the hero is a student who failed an exam. There must be beer somewhere in the game]"),
     nl, !. 
 
-check(_) :-
-    write('Nothing to check. Object is not in your possession'), nl.
+describe(_) :-
+    write('Nothing to describe. Object is not in your inventory'), nl.
 
 /* Actions with objects */
 
 grab(firefly, jar) :-
-    possess(jar),
+    holding(jar),
     write("Wow! You managed to grab a firefly! Be careful not to free it."),
     nl, !.
 
-possess(firefly) :-
+holding(firefly) :-
     grab(firefly, jar),
     nl, !.
+
+take(key) :-
+    write("Sorry, you are not able to easily pick up the key. Think of a different solution."), nl,
+    false.
+
+/* Can use be used? It's being highlighted in the editor as sth built-in... */
+use(magnet, whole, X) :-
+    metal(X),
+    holding(magnet),
+    i_am_at(Place),
+    at(X, Place),
+    retract(at(X, Place)),
+    assert(holding(X)),
+    write("Great! You managed to pick up "),
+    write(X), nl.
+
---
+++ prolog/spells.pl
@@ -0,0 +1,7 @@
+/* Spells available in the game */
+
+/* @TODO: Connect somehow with a room */
+cast(light, firefly) :=
+    holding(firefly).
+
+/* @TODO: add more spells */
