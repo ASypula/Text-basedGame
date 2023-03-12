5115beefcc20430295997fbd3cae0acd3bb725a0
Mateusz Wasilewski <01158977@pw.edu.pl>
2023-03-12 12:21:30 +0100

    Fixed typos in spells.pl

--- prolog/spells.pl
+++ prolog/spells.pl
@@ -5,13 +5,13 @@ spell(light, firefly).
 /* there will be rule named like "spell_in_room" and it will take spell name and rule. The spell_in_room would be described in each room file if there is useful spell to cast there. Default would be something like "This spell can't help you here".
 In the "cast" rule after checking if correct component is held the spell_in_room would be checked with room you are currently in as second argument. */
 
-cast(Spell_name, Component) :=
+cast(Spell_name, Component) :-
     spell(Spell_name, Component),
     holding(Component), 
     i_am_at(Place),
     cast_at_place(Spell_name, Place), !.
 
-cast(Spell_name, Component) :=
+cast(Spell_name, Component) :-
     spell(Spell_name, Component),
     \+ holding(Component), 
     write("You don't have that component"), nl, !.
