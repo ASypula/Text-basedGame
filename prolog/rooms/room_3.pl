80eae60d9aab55d9955156bc4d70c6135d25d4f2
asypula <a.sypula@stud.elka.pw.edu.pl>
2023-03-14 22:26:58 +0100

    Description in room_3 in lit version updated

--- prolog/rooms/room_3.pl
+++ prolog/rooms/room_3.pl
@@ -5,12 +5,12 @@ describe(room_3) :-
     obscured(room_3),
     write('Junk Room - The room is covered by unnatural magical darkness, your lantern cannot illuminate.'), nl,
     write('You stumble upon various object scattered on the floor, but there is no way to distinguish whether any of them may be useful.'), nl,
-    write('By touch you find passage north east and west.'), nl.
+    write('By touch you find passage north, east and west.'), nl.
 
 describe(room_3) :-
-    \+ obscured(room_3),
     write('Now, when the light provides better visibility, you can distinguish various objects littering the floor.'), nl, 
-    write('One of them in particular draws your attention... a beige pile of something but you need to look closer to name it.'), nl.
+    write('One of them in particular draws your attention... a beige pile of something but you need to look closer to name it.'), nl,
+    write('Now the possible exits are more clearly visible: three different passages leading north, east and west.'), nl.
 
 hint(room_3) :-
     write('[when you cast light you can find useful objects here]'), nl.
