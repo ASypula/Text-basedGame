a77beb61968a5d8a8975ca19a0884b763ce6663e
Byku <sztanga.eryk@gmail.com>
2023-03-15 13:59:57 +0100

    Added file with room_16 I forgot before

---
+++ prolog/rooms/room_16.pl
@@ -0,0 +1,29 @@
+/* Room description */
+describe(room_16) :-
+    write('You enter a vast, cluttered room. You have to manouver between shelves, crates and tables to move around.'), nl,
+    write('Exits are on the west and north wall.'), nl,
+    write('The room is filled with jars, bottles and glass aparature.'), nl.
+
+hint(room_16) :-
+    write('[An appropriate spell or something magnetic would help with obtaining the key.]'), nl.
+
+/* Room setup */
+at(potion, room_16).
+at(jar, room_16).
+at(key, room_16).
+
+/* rules */
+
+/* Puzzles */
+
+/* Objects */
+describe_additional(room_16) :-
+    at(potion, room_16),
+    write('Only one of the bottles appears to have something in it.'), nl, false.
+
+describe_additional(room_16) :-
+    at(key, room_16),
+    nl, 
+    write('As you look around the room you notice something shiny between floor tiles, but you cannot grab it with your fingers. '),
+    write('Some sort of jewellery? Or maybe a key?'), 
+    nl, false.
