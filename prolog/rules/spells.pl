9d299d28be65f38d8ebd6af64b4d724b217b87ee
asypula <a.sypula@stud.elka.pw.edu.pl>
2023-03-15 16:59:54 +0100

    open spell with key updated

--- prolog/rules/spells.pl
+++ prolog/rules/spells.pl
@@ -2,7 +2,6 @@
 spell(light, firefly).
 spell(open, key).
 
-/* @TODO: Connect somehow with a room */
 /* there will be rule named like "spell_in_room" and it will take spell name and rule. The spell_in_room would be described in each room file if there is useful spell to cast there. Default would be something like "This spell can't help you here".
 In the "cast" rule after checking if correct component is held the spell_in_room would be checked with room you are currently in as second argument. */
 
@@ -24,22 +23,18 @@ cast(Spell_name, _) :-
 cast(_, _) :-
     write("Don't know such spell."), nl.
 
+/* Specific spells */
+
 cast_at_place(light, Place) :-
     obscured(Place),
-    cast(light, firefly),
     i_am_at(Place),
     retract(obscured(Place)),
     write("The room has been instantaneously illuminated!"), !.
 
+cast_at_place(open, room_6) :-
+    blocked(room_5, room_6),
+    retract(blocked(room_5, room_6)),
+    write('You hear a click sound and the doors are beginning to open slowly.'), nl, !.
+
 cast_at_place(_, _) :-
     write("Not so useful here."), nl.
-
-/* Specific spells */
-/* I am not sure If we can write the cast differently - not to overwrite the function
-    If we don't check all the requirements here, then the unblock will work when we are not sure if e.g. someone is holding the key */
-cast(open, key) :-
-    i_am_at(room_6),
-    blocked(room_5, room_6),
-    holding(key),
-    retract(blocked(room_5, room_6)),
-    write('You hear a click sound and the doors are beginning to open slowly.'), nl, false.
