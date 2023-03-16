911529fffe74e045f2d393be6d06dc296522eceb
Mateusz Wasilewski <01158977@pw.edu.pl>
2023-03-16 17:06:03 +0100

    Added initial implementation of room 8

---
+++ prolog/rules/undead_student.pl
@@ -0,0 +1,33 @@
+/* These rules define interaction with the Undead Student */
+interact :-
+    i_am_at(room_8),
+    visited_previously(a),
+    retractall(visited_previously(a)),
+    assert(visited_previously(b)),
+    write('\"Oh another lost student.\" it says. \"Don''t be afraid, I was once just like you - a lost student.\"'), nl,
+    write('\"I believe we can help each other. I found many secrets in this hidden library but unfortunately became bound to it.\"'), nl,
+    write('\"I know a spell to defeat the beast but it is no use to me. I will share it with you, but first you need to bring me... a beer. As a memorial of my past.\"'), nl,
+    write('\"Some students sneak beer in here, it should be possible to find some.\"'), nl, !.
+
+interact :-
+    i_am_at(room_8),
+    visited_previously(b),
+    write('\"So, do you have it?\" Its empty eye sockets look at you menacingly.'), nl,
+    write('[To give the beer to the Undead Student use give(beer).]'), nl, !.
+
+give(beer) :-
+    i_am_at(room_8),
+    holding(beer),
+    visited_previously(b),
+    retractall(holding(beer)),
+    retractall(visited_previously(b)),
+    assert(visited_previously(c)),
+    write('\"Ah it''s been a while. I probably can''t taste it now though...\"'), nl,
+    write('\"Anyway the spell name is power_word_kill and the component is a bunch of wolfsbane.\"'), nl, !.
+
+give(X) :-
+    holding(X),
+    write('You can''t give this object away...'), nl, !.
+
+give(_) :-
+    write('How do you imagine giving away something you don''t have?'), nl, !.
