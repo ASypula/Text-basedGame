6021e4008a7f80824e101d35c32a2d75346f22aa
Mateusz Wasilewski <01158977@pw.edu.pl>
2023-05-07 11:55:15 +0200

    Cleaned up Main.hs

--- haskell/app/Main.hs
+++ haskell/app/Main.hs
@@ -38,56 +38,31 @@ readCommand = do
 
 -- note that the game loop may take the game state as
 -- an argument, eg. gameLoop :: State -> IO ()
--- gameLoop :: State -> IO ()
--- gameLoop st = do
---     cmd <- readCommand
---     case cmd of
---         ["instructions"] -> do printInstructions
---                                gameLoop st
---         ["look"] -> do describeState st
---                        gameLoop st
---         ["take", objectName'] -> do printLines(getMsgForObjectPickUp objectName' st)
---                                     let newState = takeObjectIfExists objectName' st
---                                     gameLoop newState
---         ("take": _) -> do printLines ["Correct syntax is \"take OBJECT_NAME\"."]
---                           gameLoop st
---         ["inventory"] -> do printLines(getInventoryItemsDescription st)
---                             gameLoop st
---         ["investigate"] -> do investigateObject old_journal
---                               gameLoop st
---         ["quit"] -> return ()
---         _ -> do printLines ["Unknown command.", ""]
---                 gameLoop st
+gameLoop :: State -> IO ()
+gameLoop st = do
+    cmd <- readCommand
+    case cmd of
+        ["instructions"] -> do printInstructions
+                               gameLoop st
+        ["look"] -> do describeState st
+                       gameLoop st
+        ["take", objectName'] -> do printLines(getMsgForObjectPickUp objectName' st)
+                                    let newState = takeObjectIfExists objectName' st
+                                    gameLoop newState
+        ("take": _) -> do printLines ["Correct syntax is \"take OBJECT_NAME\"."]
+                          gameLoop st
+        ["inventory"] -> do printLines(getInventoryItemsDescription st)
+                            gameLoop st
+        ["investigate"] -> do investigateObject old_journal
+                              gameLoop st
+        ["quit"] -> return ()
+        _ -> do printLines ["Unknown command.", ""]
+                gameLoop st
 
--- main = do
---     printIntroduction
---     printInstructions
---     describeState state
---     removeObjectFromRoom "lantern" "room_2" state
---     describeState state
-    -- gameLoop state
-
--- TEST for 3rd room (obscured)
 main = do
+    printIntroduction
+    printInstructions
     describeState state
-    let newState = removeAddition "obscured" state
-    describeState newState
-
--- TEST moving a player
--- main = do
---     let (newState, moved) = move E state
---     print $ room $ player $ newState
---     moveOutcome newState moved
-
--- TEST: for testing object pick up
--- main = do
---     printIntroduction
---     printInstructions
---     describeState state
---     let inventoryObjectNames = getStringInventory (player state)
---     printLines inventoryObjectNames
---     let (newState, message) = takeObjectFromRoom "old_journal" "room_2" state
---     putStrLn message
---     describeState newState
---     let inventoryObjectNames = getStringInventory (player newState)
---     printLines inventoryObjectNames
+    removeObjectFromRoom "lantern" "room_2" state
+    describeState state
+    gameLoop state
