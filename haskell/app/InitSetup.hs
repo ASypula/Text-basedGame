25edbd3f5ce8658d16ecd54ab77fc6642bfd8ea4
Byku <sztanga.eryk@gmail.com>
2023-05-09 19:51:13 +0200

    Added rooms 4 and 14

--- haskell/app/InitSetup.hs
+++ haskell/app/InitSetup.hs
@@ -39,7 +39,10 @@ hint_room16 = "[An appropriate spell or something magnetic would help with obtai
 room1 = Room {roomName = "room_1", objects = Nothing, hints = [], blocked = False, additions = []}
 room2 = Room {roomName = "room_2", objects = Just [old_journal], hints = [hint_room2], blocked = False, additions = []}
 room3 = Room {roomName = "room_3", objects = Just [rope], hints = [hint_room3], blocked = False, additions = ["obscured"]}
---TODO: room4
+room4 = Room {roomName = "room_4", objects = Nothing, hints = [], blocked = False, additions = []}
+room4N = Room {roomName = "room_4N", objects = Just [nightcap], hints = [], blocked = False, additions = []}
+room4S = Room {roomName = "room_4S", objects = Nothing, hints = [], blocked = False, additions = []}
+--TODO: write function to move objects from 4N and 4S to 4 upon unlocking
 room5 = Room {roomName = "room_5", objects = Just [note], hints = [], blocked = False, additions = []}
 room6 = Room {roomName = "room_6", objects = Just [magnet], hints = [], blocked = False, additions = []}
 room8 = Room {roomName = "room_8", objects = Nothing, hints = [hint_room8], blocked = False, additions = []}
@@ -47,7 +50,9 @@ room10 = Room {roomName = "room_10", objects = Nothing, hints = [], blocked = Fa
 room11 = Room {roomName = "room_11", objects = Just[ripped_page], hints = [], blocked = False, additions = []}
 room12 = Room {roomName = "room_12", objects = Just [wolfsbane], hints = [hint_room12], blocked = False, additions = ["obscured"]}
 room13 = Room {roomName = "room_13", objects = Just [ripped_journal], hints = [], blocked = False, additions = []}
---TODO room14
+room14 = Room {roomName = "room_14", objects = Just [cheat_sheet, beer, book_fragment], hints = [], blocked = False, additions = []}
+room14W = Room {roomName = "room_14W", objects = Nothing, hints = [], blocked = False, additions = []}
+room14S = Room {roomName = "room_14S", objects = Nothing, hints = [], blocked = False, additions = []}
 room15 = Room {roomName = "room_15", objects = Just [burned_journal], hints = [], blocked = False, additions = []}
 room16 = Room {roomName = "room_16", objects = Just [potion, key, jar], hints = [], blocked = False, additions = []}
 
@@ -57,4 +62,4 @@ roomsMap =  Map.fromList [("room_1", room1), ("room_2", room2), ("room_3", room3
                         ("room_6", room6), ("room_8", room8), ("room_10", room10), ("room_11", room11),
                         ("room_12", room12), ("room_13", room13), ("room_15", room15), ("room_16", room16)]
 -- TODO: additional rooms to roomsMap
-state = State{player = gamer, rooms=roomsMap, blocades=["room_4"]}
+state = State{player = gamer, rooms=roomsMap, blocades=["room_4", "room_14", "room_12", "room_13", "room_5"]}
